da1bdf4db8c0aea34ef8795826f655c607c4fb4d
Jamba <happyjuice101@gmail.com>
2023-12-01 21:03:31 -0800

    Updated particle.cpp

--- code/Particle.cpp
+++ code/Particle.cpp
@@ -38,8 +38,36 @@ void Particle::draw(RenderTarget& target, RenderStates states) const
     VertexArray lines(TriangleFan, m_numPoints + 1);
     Vector2f center = target.mapCoordsToPixel(m_centerCoordinate, m_cartesianPlane);
     
+    lines[0].position = center;
+    lines[0].color = m_color1;
+
+    for (int j = 1; j < m_numPoints; j++)
+    {
+        lines[j].position = target.mapCoordsToPixel(m_A(0, j - 1), m_cartesianPlane);
+        lines[j].color = m_color2;
+
+        target.draw(lines);
+    }
 }
 
+void Particle::update(float dt)
+{
+    m_ttl -= dt;
+
+    rotate(dt * m_radiansPerSec);
+    scale(SCALE);
+
+    float dx = m_vx * dt;
+    m_vy -= G * dt;
+    float dy = m_vy * dt;
+
+    translate(dx, dy);
+}
+
+void Particle::translate(double xShift, double yShift)
+{
+
+}
 
 bool Particle::almostEqual(double a, double b, double eps)
 {
