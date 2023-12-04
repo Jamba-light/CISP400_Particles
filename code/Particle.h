66a536acd26a4e72a2e712204d554553c8075640
Jamba <happyjuice101@gmail.com>
2023-12-03 19:54:09 -0800

    Updated source files

--- code/Particle.h
+++ code/Particle.h
@@ -1,8 +1,9 @@
 #pragma once
+#define _USE_MATH_DEFINES
 #include "Matrices.h"
 #include <SFML/Graphics.hpp>
 
-const float M_PI = 3.1415926535897932384626433;
+//const float M_PI = 3.1415926535897932384626433;
 const float G = 1000;      //Gravity
 const float TTL = 5.0;  //Time To Live
 const float SCALE = 0.999;
